d08ebc16ca347fa22e923ca4e0da3bc0c454210b
Jonathan Ramos Pereira <desenvjonathan@hotmail.com>
2022-11-16 13:17:34 -0300

    finished game

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const randomNumber = Math.round(Math.random() * 10)
+let xAttempts = 1;
+
 function handleClick(event) {
   event.preventDefault()
 
   const inputNumber = document.querySelector("#inputNumber")
-  console.log(inputNumber.value)
+
+  if(Number(inputNumber.value) == randomNumber) {
+    document.querySelector(".screen1").classList.add("hide")
+    document.querySelector(".screen2").classList.remove("hide")
+
+    document
+      .querySelector(".screen2 h2")
+      .innerText = `Acertou em ${xAttempts} tentativas`
+  }
+
+  xAttempts++
 }
